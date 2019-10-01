819e845a2d09a07ca3efc7ecce802f12217d570f
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-01 14:05:08 -0300

    feat(backend): first commit with minimal express+mongoose setup

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://airbnc:airbnc@airbnc-7iugc.mongodb.net/airbnc?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json);
+app.use(routes);
+
+app.get('/', (req, res) => {
+  return res.json('Hellow');
+});
+
+app.listen(8082);
