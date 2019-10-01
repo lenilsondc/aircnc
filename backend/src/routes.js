36a96cfe44a94b6c14b34129051dd3bd9c55ea5c
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-01 20:00:40 -0300

    feat(backend): add spot store method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
 const SessionController = require("./controller/SessionController");
+const SpotController = require("./controller/SpotController");
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 module.exports = routes;
