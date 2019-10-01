37c96ed17edf787e46c7a61256807a278a385a5b
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-01 20:06:01 -0300

    feat(backend): add index spots method

--- backend/src/controller/SpotController.js
+++ backend/src/controller/SpotController.js
@@ -2,6 +2,12 @@ const Spot = require("../models/Spot");
 const User = require("../models/User");
 
 module.exports = {
+  async index(req, res) {
+    const { tech } = req.query;
+    const spots = await Spot.find({ techs: tech });
+
+    return res.json(spots);
+  },
   async store(req, res) {
     const { filename } = req.file;
     const { company, price, techs } = req.body;
