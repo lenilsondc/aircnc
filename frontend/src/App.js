77ffa79a7473f8a8daca508bac4ebb0832a801d7
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-02 18:59:23 -0300

    feat(frontend): add routes to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 import logo from "./assets/logo.svg";
-import api from "./services/api";
+import Routes from "./routes";
 
 function App() {
-  const [email, setEmail] = useState("");
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const response = await api.post("/sessions", { email });
-
-    const { _id } = response.data;
-
-    localStorage.setItem("user", _id);
-  }
-
   return (
     <div className="container">
       <img src={logo} />
-
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para a sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL*</label>
-          <input
-            id="name"
-            type="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-          <button className="btn" type="sumbit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
