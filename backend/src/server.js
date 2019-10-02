6735181c195cb626f948ba476fd4eb5b39c48dc0
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-02 19:17:47 -0300

    feat(backend): add thumbnail_url to resolve files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const routes = require('./routes');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 mongoose.connect(
-  'mongodb+srv://airbnc:airbnc@airbnc-7iugc.mongodb.net/airbnc?retryWrites=true&w=majority',
+  "mongodb+srv://airbnc:airbnc@airbnc-7iugc.mongodb.net/airbnc?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -15,10 +16,11 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.get('/', (req, res) => {
-  return res.json('Hellow');
+app.get("/", (req, res) => {
+  return res.json("Hellow");
 });
 
 app.listen(8082);
