210078230a048d7f4a791b0e984d1b6e0f631b6b
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-02 14:01:19 -0300

    feat(frontend): add login page form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      Hello, world!
+    <div className="container">
+      <img src={logo} />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL*</label>
+          <input type="email" id="name" placeholder="Seu melhor e-mail" />
+          <button className="btn" type="sumbit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
