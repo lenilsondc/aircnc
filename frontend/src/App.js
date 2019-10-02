b47f58885b7e7349bb901f23dd062e3d8be5d0a2
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-02 18:47:59 -0300

    feat(fronend): connect login to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import logo from './assets/logo.svg';
+import "./App.css";
+import logo from "./assets/logo.svg";
+import api from "./services/api";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post("/sessions", { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem("user", _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} />
 
       <div className="content">
         <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          <strong>talentos</strong> para a sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL*</label>
-          <input type="email" id="name" placeholder="Seu melhor e-mail" />
+          <input
+            id="name"
+            type="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
           <button className="btn" type="sumbit">
             Entrar
           </button>
