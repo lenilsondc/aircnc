86a0c940dead42cf2b7b36dd92c4411d90b37a3e
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-04 19:17:00 -0300

    feat(backend): add websocket support to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,12 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://airbnc:airbnc@airbnc-7iugc.mongodb.net/airbnc?retryWrites=true&w=majority",
@@ -14,9 +18,23 @@ mongoose.connect(
   }
 );
 
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(8082);
+server.listen(8082);
