1c42130a257b5e261a540c6aec175733c9c5e594
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-03 23:02:32 -0300

    fix(mobile): add manual safe area for android devices

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { View, StyleSheet, Platform } from "react-native";
 
 import Routes from "./src/routes";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <View style={styles.main}>
+      <Routes />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 25 : 0
+  }
+});
