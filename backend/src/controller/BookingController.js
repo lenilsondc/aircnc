003da9a2fb955fcc935dfd1d2110851ac6b0e9e1
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-03 22:45:24 -0300

    fix(backend): fix call to request param

--- backend/src/controller/BookingController.js
+++ backend/src/controller/BookingController.js
@@ -3,8 +3,8 @@ const Booking = require("../models/Booking");
 module.exports = {
   async store(req, res) {
     const { user_id } = req.headers;
-    const { spot_id } = route.params;
-    const { date } = route.body;
+    const { spot_id } = req.params;
+    const { date } = req.body;
 
     const booking = await Booking.create({
       user: user_id,
