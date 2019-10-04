5dcec54d538704bc769d06c66007dfd1356e017c
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-03 22:19:12 -0300

    feat(mobile): create list of spots by tech page ui

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,47 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  Image,
+  ScrollView,
+  StyleSheet,
+  AsyncStorage
+} from "react-native";
+
+import logo from "../assets/logo.png";
+import SpotList from "../components/SpotList";
 
 export default function List() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(techs => {
+      const techsArray = techs.split(",").map(x => x.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map((tech, i) => (
+          <SpotList key={i} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    paddingTop: 10
+  }
+});
