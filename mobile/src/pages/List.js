46b6324ed1d8d48e245598fe27a448753d9bdde2
lenilsondc <lenilson.decastro@yahoo.com>
2019-10-04 22:11:01 -0300

    feat(mobile): add display alert dialog when socket notifies with approval or rejection on booking a spot

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -4,8 +4,10 @@ import {
   Image,
   ScrollView,
   StyleSheet,
-  AsyncStorage
+  AsyncStorage,
+  Alert
 } from "react-native";
+import socketio from "socket.io-client";
 
 import logo from "../assets/logo.png";
 import SpotList from "../components/SpotList";
@@ -18,6 +20,22 @@ export default function List() {
       const techsArray = techs.split(",").map(x => x.trim());
 
       setTechs(techsArray);
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.0.103:8082", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
     });
   }, []);
 
